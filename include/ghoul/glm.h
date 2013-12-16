b4ca10a416d4368665ffe939433dbb755e15dc82
Alexander Bock <alexander.bock@liu.se>
2013-12-16 18:36:54 +0100

    add common glm.h header file removing clang warnings

---
+++ include/ghoul/glm.h
@@ -0,0 +1,39 @@
+/**************************************************************************************************
+ *                                                                                                *
+ * GLM                                                                                            *
+ *                                                                                                *
+ * Copyright (c) 2012 Alexander Bock                                                              *
+ *                                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software  *
+ * and associated documentation files (the "Software"), to deal in the Software without           *
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,     *
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the  *
+ * Software is furnished to do so, subject to the following conditions:                           *
+ *                                                                                                *
+ * The above copyright notice and this permission notice shall be included in all copies or       *
+ * substantial portions of the Software.                                                          *
+ *                                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING  *
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND     *
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,   *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        *
+ *************************************************************************************************/
+
+#ifndef __GLM_H__
+#define __GLM_H__
+
+#ifdef __APPLE__
+// The GLM header throw 'anonymous struct' warnings that we do not want. By marking the following
+// files as system headers, all warnings are ignored
+#pragma clang diagnostic push
+#pragma clang system_header
+#endif
+
+#include <glm/glm.hpp>
+
+#ifdef __APPLE__
+#pragma clang diagnostic pop
+#endif
+
+#endif // __GLM_H__
