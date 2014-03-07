20d0de41d5d01ec2fe4b440d53903901a952cd55
Alexander Bock <alexander.bock@liu.se>
2014-03-07 18:32:54 +0100

    Enable the TemplateFactory to deal with the creation of classes that take a ghoul::Dictionary in their constructor

--- include/ghoul/misc/templatefactory.h
+++ include/ghoul/misc/templatefactory.h
@@ -28,33 +28,47 @@
 
 #include <map>
 #include <string>
+#include <functional>
+
+#include <ghoul/misc/dictionary.h>
 
 namespace ghoul {
 
 /**
  * This class implements a generic Factory pattern that can be used for any class as long
- * as a default constructor is available for each subclass. The usage of this
- * TemplateFactory is as follows: The <code>BaseClass</code> template parameter is the
- * base class of all classes that can be created using this factory. Only subclasses of
- * <code>BaseClass> can be added to the factory, or a compile-time error will be
- * generated. Subclasses of the <code>BaseClass</code> (or the <code>BaseClass</code>
- * itself) can be registered using the #registerClass method, which takes the subclass as
- * a template parameter. All registered classes can then be created using the #create
- * method, providing the same <code>className</code> with which the class was registered.
- * #hasClass tests if a specific <code>className</code> was registered previously. For
- * example:
+ * as a default constructor and/or constructor taking a #Dictionary is available for the
+ * subclass. The usage of this TemplateFactory is as follows: The <code>BaseClass</code>
+ * template parameter is the base class of all classes that can be created using this
+ * factory. Only subclasses of <code>BaseClass> can be added to the factory, or a
+ * compile-time error will be generated. Subclasses of the <code>BaseClass</code> (or the
+ * <code>BaseClass</code> itself) can be registered using the #registerClass method, which
+ * takes the subclass as a template parameter. All registered classes can then be created
+ * using the #create method, providing the same <code>className</code> with which the
+ * class was registered. If the subclass that was registered provides both an empty
+ * default constructor as well as a constructor taking a single
+ * <code>const ghoul::Dictionary&</code> parameter, the second #create method can be used
+ * which takes the <code>className</code> as well as the #Dictionary with which to
+ * instantiate the class. If this method is used and the class does not provide a
+ * #Dictionary constructor, an error is logged (if the source was compiled with
+ * <code>GHL_DEBUG</code>). In any case, the #Dictionary is silently ignored. #hasClass
+ * tests if a specific <code>className</code> was registered previously. For example:
  * \verbatim
 class A {};
 class B : public A {};
 class C {};
+class D : public A {
+    D(const ghoul::Dictionary&);
+};
 
 TemplateFactory<A> factory;
 factory.registerClass<B>("B");
-A* instance = factory.create("B");
+factory.registerClass<C>("C"); // compile error as C is not a subclass of A
+factory.registerClass<D>("D");
+A* i = factory.create("B"); // creates an instance of B with the default constructor
+i = factory.create("D"); // creates an instance of D with the default constructor
+i = factory.create("D", { }); // creates an instance of D with the Dictionary constructor
+i = factory.create("B", { }); // will log an error as B does not have a Dictionary ctor
 \endverbatim
- * Will create a correct instance of <code>B</code>, while
- * <code>factory.registerClass<C>("C")</code> will not compile, as <code>C</code> is not a
- * subclass of <code>A</code> with which the <code>factory</code> was created.
  * \tparam BaseClass The base class of all classes that can be registered and created
  * using this factory
  */
@@ -72,6 +86,21 @@ public:
      * <code>nullptr</code> if <code>className</code> did not name a valid type
      */
     BaseClass* create(const std::string& className) const;
+
+    /**
+    * Creates an instance of the class which was registered under the provided
+    * <code>className</code>. This creation uses the constructor of the class and passes
+    * the <code>dictionary</code> to the constructor. For this method to work, the class
+    * which was registered with <code>className</code> has to have a constructor with a
+    * single #Dictionary as parameter. If <code>className</code> does not name a
+    * registered class, a <code>nullptr</code> is returned and an error is logged. Classes
+    * can be registered with the #registerClass method.
+    * \param className The class name of the instance that should be created
+    * \param dictionary The dictionary that will be passed to the constructor of the class
+    * \return A fully initialized instance of the registered class, or
+    * <code>nullptr</code> if <code>className</code> did not name a valid type
+    */
+    BaseClass* create(const std::string& className, const Dictionary& dictionary) const;
 
     /**
      * Registers a <code>Class</code> with the provided <code>className</code> so that it
@@ -102,7 +131,9 @@ private:
      * The function pointer that is stored in the map. This pointer points to a templated
      * function, which is templated by the destination type.
      */
-    typedef BaseClass* (*FactoryFuncPtr)();
+    typedef BaseClass* (*FactoryFuncPtr)(bool, const Dictionary&);
+
+    //typedef std::function<BaseClass*(bool, const ghoul::Dictionary&)> FactoryFuncPtr;
 
     /// The map storing all the associations from <code>className</code> to classes
     std::map<std::string, FactoryFuncPtr> _map;
