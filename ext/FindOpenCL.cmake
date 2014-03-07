71f811bcbb8c2809120b9d8b12525bd29d035687
Alexander Bock <alexander.bock@liu.se>
2014-03-07 16:48:50 +0100

    Fix FindOpenCL for Windows and x86 vs x64 installation Adapt FindOpenCL to use default CMake module variables Make CMakeLists.txt use the correct variables

--- ext/FindOpenCL.cmake
+++ ext/FindOpenCL.cmake
@@ -6,8 +6,8 @@
 # OPENCL_FOUND - if the library was successfully located
 
 if(WIN32)
-	find_path(OPENCL_INCLUDES CL/cl.h)
-
+	#find_path(OPENCL_INCLUDES CL/cl.h)
+    
     # The AMD SDK currently installs both x86 and x86_64 libraries
     # This is only a hack to find out architecture
     if( ${CMAKE_SYSTEM_PROCESSOR} STREQUAL "AMD64" )
@@ -34,20 +34,20 @@ if(WIN32)
     find_path(_OPENCL_CPP_INCLUDE_DIRS CL/cl.hpp PATHS "${_OPENCL_INC_CAND}" $ENV{CUDA_INC_PATH} $ENV{CUDA_PATH}/include)
 endif(WIN32)
 if (APPLE)
-	find_path(OPENCL_INCLUDES OpenCL/cl.h)
-    find_library(FRAMEWORK_OpenCL
+	find_path(OPENCL_INCLUDE_DIRS OpenCL/cl.h)
+    find_library(OPENCL_LIBRARIES
         NAMES OpenCL
         PATHS ${CMAKE_OSX_SYSROOT}/System/Library
         PATH_SUFFIXES Frameworks
         NO_DEFAULT_PATH
     )
-    set(OPENCL_LIBRARY ${FRAMEWORK_OpenCL})
+#    set(OPENCL_LIBRARY ${FRAMEWORK_OpenCL})
 endif(APPLE)
 if(UNIX AND NOT APPLE)
-	find_path( OPENCL_INCLUDES CL/cl.h
+	find_path( OPENCL_INCLUDE_DIRS CL/cl.h
 		HINTS 	"/usr/include"
 			"/usr/local/include")
-	find_library(	OPENCL_LIBRARY
+	find_library(	OPENCL_LIBRARIES
 		NAMES ${CMAKE_DYNAMIC_LIBRARY_PREFIX}OpenCL${CMAKE_DYNAMIC_LIBRARY_SUFFIX}
 					HINTS 	"/lib"
 							"/usr/lib"
@@ -55,19 +55,13 @@ if(UNIX AND NOT APPLE)
 	)
 endif(UNIX AND NOT APPLE)
 
-MARK_AS_ADVANCED(OPENCL_INCLUDES OPENCL_LIBRARY)
-
-set(OPENCL_INCLUDE_DIRECTORIES ${OPENCL_INCLUDES})
-set(OPENCL_LIBRARIES ${OPENCL_LIBRARY} ${OPENCL_DEPENDENCIES})
+MARK_AS_ADVANCED(OPENCL_INCLUDE_DIRS OPENCL_LIBRARIES)
 
 
 # handle the QUIETLY and REQUIRED arguments and set OPENCL_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(OPENCL  DEFAULT_MSG
-                                  OPENCL_LIBRARY OPENCL_INCLUDES)
-
-if(OPENCL_FOUND) 
-    MESSAGE(STATUS "OpenCL found: ${OPENCL_INCLUDE_DIRECTORIES}/sgct.h")
-else()
+                                  OPENCL_LIBRARIES OPENCL_INCLUDE_DIRS)
+if(NOT OPENCL_FOUND) 
     MESSAGE(FATAL_ERROR "OpenCL not found!")
-endif(OPENCL_FOUND)
+endif(NOT OPENCL_FOUND)
