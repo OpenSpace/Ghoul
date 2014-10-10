157c3027e803a8e894ed09ccec949da7c09670b5
Alexander Bock <alexander.bock@liu.se>
2014-10-10 17:02:01 +0200

    Commented CacheManager Removed method to check if a cached file is available Added method to remove cached file

--- include/ghoul/filesystem/cachemanager.h
+++ include/ghoul/filesystem/cachemanager.h
@@ -28,30 +28,149 @@
 
 #include <ghoul/filesystem/directory.h>
 
+#include <ghoul/filesystem/file.h>
+
 #include <map>
 #include <string>
 
 namespace ghoul {
 namespace filesystem {
 
+/**
+ * The CacheManager allows users to request a storage location for an, optionally
+ * persistent, file path to store a cached result. This class only generates and manages
+ * the file paths and does not do any caching itself. The use case for this is are
+ * two-fold:<br>
+ * First, expensive operation that has a result, which gets written to a file and the
+ * developer wants to retain the results without recomputing it at every application
+ * start. For this, a cache file is requested with the getCachedFile method and the
+ * <code>isPersistent</code> flag set to <code>true</code>. Using the same
+ * <code>file</code> and <code>information</code> values, the same path will be retrieved
+ * in subsequent application runs.<br>
+ * The second use-case is a temporary file, also with the getCachedFile method, but the
+ * <code>isPersistent</code> flag set to <code>false</code>. Non-persistent files will
+ * automatically be deleted when the program ends.<br>
+ * The persistent files are stored in a <code>cache</code> file so that they can be
+ * retained between application runs.
+ */
 class CacheManager {
 public:
+    /**
+     * The constructor will automatically register all persistent cache entries from
+     * previous application runs and clean the directory of non-persistent entries that
+     * might have been left intact if the previous run crashed. After the constructor
+     * returns, the CacheManager will leave a cleaned cache directory and the persistent
+     * files are correctly registered and available.
+     */
 	CacheManager(std::string directory);
+    
+    /**
+     * The destructor will save all information on persistent files in a
+     * <code>cache</code> file in the cache directory that was passed in the constructor
+     * so that they can be retrieved when the application is started up again. All
+     * non-persistent files are automatically deleted in the destructor.
+     */
 	~CacheManager();
 
-	bool getCachedFile(std::string file, std::string information,
-		std::string& cachedFileName, bool isPersistent = false);
-	bool hasCachedFile(const std::string& file, const std::string& information) const;
+    /**
+     * Returns the path to a storage location for the cached file. Depending on the
+     * persistency (<code>isPersistent</code>), the directory and files will automatically
+     * be cleaned on application end or be made available automatically on the next
+     * application run. Subsequent calls (in the same run or different) with the same
+     * <code>file</code> and <code>information</code> will consistently produce the same
+     * file path. The combination of <code>file</code> and <code>information</code> is the
+     * unique key for the returned cached file. If the cached file was created before, the
+     * <code>isPersistent</code> parameter is silently ignored.
+     * \param file The file name of the file for which the cached entry is to be retrived
+     * \param information Additional information that is used to uniquely identify the
+     * cached file. This can be, for example, resolutions or parameters. The combination
+     * of the <code>file</code> + <code>information</code> uniquely identifies a cached
+     * file
+     * \param cachedFileName The output file name pointing to the cached file that can be
+     * used by the caller to store the results
+     * \param isPersistent This parameter will only be used if the cached file is used for
+     * the first time and determines if the CacheManager should automatically delete the
+     * file when the application closes (<code>false</code>) or if the file should be kept
+     * and automatically be re-added to the CacheManager on the next application run
+     * (<code>true</code>). If the cached file has been created before, this parameter is
+     * silently ignored.
+     * \return <code>true</code> if a file name was returned successfully,
+     * <code>false</code> otherwise
+     */
+    bool getCachedFile(const File& file, const std::string& information,
+                       std::string& cachedFileName, bool isPersistent = false);
 
+    /**
+     * Returns the path to a storage location for the cached file. Depending on the
+     * persistency (<code>isPersistent</code>), the directory and files will automatically
+     * be cleaned on application end or be made available automatically on the next
+     * application run. Subsequent calls (in the same run or different) with the same
+     * <code>baseName</code> and <code>information</code> will consistently produce the
+     * same file path. The combination of <code>baseName</code> and
+     * <code>information</code> is the unique key for the returned cached file. If the
+     * cached file was created before, the <code>isPersistent</code> parameter is silently
+     * ignored.<br>
+     * As the <code>baseName</code> will be used as a name for a directory in the file
+     * system, the usual restrictions apply. The baseName is automatically converted into
+     * lower case, so that the <code>baseName</code> of <code>base</code>,
+     * <code>bAsE</code>, and <code>BASE</code> all refer to the same file. Furthermore,
+     * the <code>baseName</code> cannot contain any of the following characters:
+     * <code>/</code>, <code>\\</code>, <code>?</code>, <code>%</code>, <code>*</code>,
+     * <code>:</code>, <code>|</code>, <code>"</code>, <code>\<</code>, <code>\></code>,
+     * or <code>.</code>
+     * \param baseName The base name for which the cached entry is to be retrived. A list
+     * of restrictions apply to this base name (see above)
+     * \param information Additional information that is used to uniquely identify the
+     * cached file. This can be, for example, resolutions or parameters. The combination
+     * of the <code>baseName</code> + <code>information</code> uniquely identifies a
+     * cached file
+     * \param cachedFileName The output file name pointing to the cached file that can be
+     * used by the caller to store the results
+     * \param isPersistent This parameter will only be used if the cached file is used for
+     * the first time and determines if the CacheManager should automatically delete the
+     * file when the application closes (<code>false</code>) or if the file should be kept
+     * and automatically be re-added to the CacheManager on the next application run
+     * (<code>true</code>). If the cached file has been created before, this parameter is
+     * silently ignored.
+     * \return <code>true</code> if a file name was returned successfully,
+     * <code>false</code> otherwise
+     */
+    bool getCachedFile(const std::string& baseName, const std::string& information,
+		std::string& cachedFileName, bool isPersistent = false);
+    
+    /**
+     * Removes the cached file and deleted the entry from the CacheManager. If the
+     * <code>file</code> has not previously been used to request a cache entry, no error
+     * will be signaled.
+     * \param file The file for which the cache file should be deleted
+     * \param information The detailed information for the cached file which should be
+     * deleted
+     */
+    void removeCacheFile(const File& file, const std::string& information);
+
+    /**
+     * Removes the cached file and deleted the entry from the CacheManager. If the
+     * <code>baseName</code> has not previously been used to request a cache entry, no
+     * error will be signaled. The same restrictions for the <code>baseName</code> as in
+     * getCachedFile apply to this function.
+     * \param baseName The base name for which the cache file should be deleted
+     * \param information The detailed information identifying the cached file that
+     * should be deleted
+     */
+void removeCacheFile(const std::string& baseName,
+                         const std::string& information);
+    
 protected:
+    /// This struct stores the cache information for a specific hash value.
 	struct CacheInformation {
-		std::string file;
-		std::string information;
-		bool isPersistent;
+        std::string file; ///< The path to the cached file
+        bool isPersistent; ///< if the cached entry should be automatically deleted
 	};
 
 	unsigned int generateHash(std::string file, std::string information) const;
 	void cleanDirectory(const Directory& dir) const;
+    std::vector<std::pair<unsigned int, std::string>> cacheInformationFromDirectory(
+                                                            const Directory& dir) const;
 
 	CacheManager(const CacheManager& c) = delete;
 	CacheManager(CacheManager&& m) = delete;
--- src/filesystem/cachemanager.cpp
+++ src/filesystem/cachemanager.cpp
@@ -26,13 +26,13 @@
 #include <ghoul/filesystem/cachemanager.h>
 
 #include <ghoul/filesystem/filesystem.h>
-#include <ghoul/filesystem/file.h>
 #include <ghoul/logging/logmanager.h>
 #include <ghoul/misc/crc32.h>
 
 #include <algorithm>
 #include <assert.h>
 #include <fstream>
+#include <string>
 
 namespace {
 	const std::string _loggerCat = "CacheManager";
@@ -44,13 +44,29 @@ namespace ghoul {
 namespace filesystem {
 
 CacheManager::CacheManager(std::string directory)
-	: _directory(directory)
+    : _directory(std::move(directory))
 {
+    // In the cache state, we check our cache directory for all values, in a later step
+    // we remove all persistent values, so that only the non-persistent values remain
+    // Under normal operation, the resulting vector should be of size == 0, but if the
+    // last execution of the application crashed, the directory was not cleaned up
+    // properly
+    std::vector<std::pair<unsigned int, std::string>> cacheState =
+                                                cacheInformationFromDirectory(_directory);
+
 	std::string&& path = FileSys.pathByAppendingComponent(_directory, _cacheFile);
 	std::ifstream file(path);
 	if (file.good()) {
 		std::string line;
+        // loading the cache file that might exist from a previous run of the application
+        // and reading all the persistent files from that file
 		while (std::getline(file, line)) {
+            // The structure of the cache file is assumed as follows (writting in the
+            // destructor call of the application's previous run):
+            // for each file:
+            //   hash number\n
+            //   filepath\n
+            //   information\n
 			std::stringstream s(line);
 
 			unsigned int hash;
@@ -58,28 +74,45 @@ CacheManager::CacheManager(std::string directory)
 
 			CacheInformation info;
 			std::getline(file, info.file);
-			std::getline(file, info.information);
 			info.isPersistent = true;
 			_files.emplace(hash, info);
+            
+            // If the current hash + file is contained in the cache state, we have to
+            // remove it
+            cacheState.erase(std::remove_if(cacheState.begin(), cacheState.end(),
+                            [&info, hash](const std::pair<unsigned int, std::string>& i) {
+                                return hash == i.first && info.file == i.second;
+                            }), cacheState.end());
 		}
 	}
+    
+    // At this point all values that remain in the cache state vector are left from a
+    // previous crash of the application
+    if (!cacheState.empty()) {
+        LINFO("There was a crash in the previous run and it left the cache unclean. "
+              "Cleaning it now");
+        for (auto cache : cacheState) {
+            LINFO("Deleting file '" << cache.second << "'");
+            FileSys.deleteFile(cache.second);
+        }
+        cleanDirectory(_directory);
+    }
 }
 
 CacheManager::~CacheManager() {
 	std::string&& path = FileSys.pathByAppendingComponent(_directory, _cacheFile);
 	std::ofstream file(path, std::ofstream::out);
 	if (file.good()) {
-		// Delete all the non-persistent files
-		// Save the persistent files in a cache file
 		for (auto p : _files) {
 			if (!p.second.isPersistent) {
+                // Delete all the non-persistent files
 				if (FileSys.fileExists(p.second.file))
 					FileSys.deleteFile(p.second.file);
 			}
 			else
+                // Save the persistent files in the cache file
 				file << p.first << std::endl <<
-						p.second.file << std::endl <<
-						p.second.information << std::endl; 
+						p.second.file << std::endl;
 		}
 	}
 	else
@@ -88,47 +121,93 @@ CacheManager::~CacheManager() {
 	cleanDirectory(_directory);
 }
 
-bool CacheManager::getCachedFile(std::string file, std::string information,
-	std::string& cachedFileName, bool isPersistent)
+bool CacheManager::getCachedFile(const File& file, const std::string& information,
+                                 std::string &cachedFileName, bool isPersistent)
 {
-	unsigned int hash = generateHash(file, information);
+    return getCachedFile(file.filename(), information, cachedFileName, isPersistent);
+}
+    
+bool CacheManager::getCachedFile(const std::string& baseName,
+                                 const std::string& information,
+                                 std::string& cachedFileName, bool isPersistent)
+{
+    size_t pos = baseName.find_first_of("/\\?%*:|\"<>.");
+    if (pos != std::string::npos) {
+        LERROR("Base name '" << baseName << "' consists of illegal character");
+        return false;
+    }
+    
+	unsigned int hash = generateHash(baseName, information);
 
 	auto it = _files.find(hash);
 	if (it != _files.end()) {
+        // If we find the hash, it has been created before and we can just return the
+        // file name to the caller
 		cachedFileName = it->second.file;
 		return true;
 	}
 	
-	File isPer(file);
-	std::string filename = isPer.filename();
-	std::string destinationBase = FileSys.pathByAppendingComponent(_directory, filename);
+    // If we couldn't find the file, we have to generate a directory with the name of the
+    // hash and return the full path containing of the cache path + requested filename +
+    // hash value
+	std::string destinationBase = FileSys.pathByAppendingComponent(_directory, baseName);
 
+    // If this is the first time anyone requests a cache for the file name, we have to
+    // create the base directory under the cache directory
 	if (!FileSys.directoryExists(destinationBase))
 		FileSys.createDirectory(destinationBase);
 
 	std::string destination = FileSys.pathByAppendingComponent(destinationBase,
 		std::to_string(hash));
+    
+    // The new destination should always not exist, since we checked before if we have the
+    // value in the map and only get here if it isn't; persistent cache entries are always
+    // in the map and non-persistent entries should have been deleted on application close
 	if (!FileSys.directoryExists(destination))
 		FileSys.createDirectory(destination);
+    else {
+        LERROR("Cache directory '" << destination << "' already existed even though it "
+               "was not registered before");
+        return false;
+    }
 
-	cachedFileName = FileSys.pathByAppendingComponent(destination, filename);
+    // Generate and output the newly generated cache name
+	cachedFileName = FileSys.pathByAppendingComponent(destination, baseName);
 
+    // Store the cache information in the map
 	CacheInformation info = {
 		cachedFileName,
-		information,
 		isPersistent
 	};
 	_files.emplace(hash, info);
 	return true;
 }
 
-bool CacheManager::hasCachedFile(const std::string& file,
-	const std::string& information) const
-{
-	unsigned int hash = generateHash(file, information);
-	return (_files.find(hash) != _files.end());
+void CacheManager::removeCacheFile(const File& file, const std::string& information) {
+    removeCacheFile(file.filename(), information);
 }
 
+void CacheManager::removeCacheFile(const std::string& baseName,
+                         const std::string& information)
+{
+    size_t pos = baseName.find_first_of("/\\?%*:|\"<>.");
+    if (pos != std::string::npos) {
+        LERROR("Base name '" << baseName << "' consists of illegal character");
+        return;
+    }
+    
+    unsigned int hash = generateHash(baseName, information);
+
+    auto it = _files.find(hash);
+    if (it != _files.end()) {
+        // If we find the hash, it has been created before and we can just return the
+        // file name to the caller
+        const std::string& cachedFileName = it->second.file;
+        FileSys.deleteFile(cachedFileName);
+        _files.erase(it);
+    }
+}
+    
 unsigned int CacheManager::generateHash(std::string file, std::string information) const
 {
 	std::string hashString = file + _hashDelimiter + information;
@@ -138,15 +217,69 @@ unsigned int CacheManager::generateHash(std::string file, std::string informatio
 }
 
 void CacheManager::cleanDirectory(const Directory& dir) const {
-	std::vector<std::string> contents = dir.read();
+    LDEBUG("Cleaning directory '" << dir << "'");
+    // First search for all subdirectories and call this function recursively on them
+	std::vector<std::string> contents = dir.readDirectories();
 	for (auto content : contents) {
-		if (FileSys.directoryExists(content))
+        if (FileSys.directoryExists(content)) {
 			cleanDirectory(content);
+        }
 	}
+    // We get to this point in the recursion if either all subdirectories have been
+    // deleted or there exists a file somewhere in the directory tree
 
 	contents = dir.read();
-	if (contents.size() == 0)
+    bool isEmpty = contents.size() == 0;
+#ifdef __APPLE__
+    // Apple stores the .DS_Store directory in the directory which can be removed
+    std::string dsStore = FileSys.pathByAppendingComponent(dir, ".DS_Store");
+    isEmpty |= ((contents.size() == 1) && contents[0] == dsStore);
+#endif
+    // If this directory is empty, we can delete it
+    if (isEmpty) {
+        LDEBUG("Deleting directory '" << dir << "'");
 		FileSys.deleteDirectory(dir);
+    }
+}
+    
+std::vector<std::pair<unsigned int, std::string>> CacheManager::cacheInformationFromDirectory(
+    const Directory& dir) const
+{
+    std::vector<std::pair<unsigned int, std::string>> result;
+    std::vector<std::string> directories = dir.readDirectories(false);
+    for (auto directory : directories) {
+        Directory d(directory);
+
+        // Extract the name of the directory
+        // +1 as the last path delimiter is missing from the path
+        std::string directoryName = directory.substr(dir.path().size() + 1);
+        
+        std::vector<std::string> hashes = d.readDirectories();
+        for (auto hash : hashes) {
+            // Extract the hash from the directory name
+            // +1 as the last path delimiter is missing from the path
+            std::string hashName = hash.substr(d.path().size() + 1);
+            
+            std::vector<std::string> files = Directory(hash).readFiles();
+            // Cache directories should only contain a single file with the
+            // same name as the directory
+            if (files.size() > 1)
+                LERROR("Directory '" << hash << "' contained more than one file");
+            if (files.size() == 1) {
+                // Extract the file name from the full path
+                // +1 as the last path delimiter is missing from the path
+                std::string filename = files[0].substr(Directory(hash).path().size() + 1);
+                if (filename != directoryName)
+                    LERROR("File contained in cache directory '" <<
+                           hash << "' contains a file with name '" << filename <<
+                           "instead of the expected '" << directoryName << "'");
+                else
+                    result.push_back({std::stoi(hashName), files[0]});
+            }
+        }
+    }
+    
+    return std::move(result);
 }
 
 } // namespace filesystem
