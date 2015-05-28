1b751cb854857effa7ba7648476633913bb60d6c
Alexander Bock <alexander.bock@liu.se>
2015-05-28 22:34:07 +0200

    Updated Ghoul header in multiple files

--- include/ghoul/misc/buffer.h
+++ include/ghoul/misc/buffer.h
@@ -1,8 +1,9 @@
 /*****************************************************************************************
  *                                                                                       *
- * owl                                                                                   *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
  *                                                                                       *
- * Copyright (c) 2014 Jonas Strandstedt                                                  *
+ * Copyright (c) 2012-2015                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
@@ -20,6 +21,8 @@
  * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ *****************************************************************************************
+ * The original of this file is found in the owl library maintained by Jonas Strandstedt *
  ****************************************************************************************/
 
 #ifndef __BUFFER_H__
