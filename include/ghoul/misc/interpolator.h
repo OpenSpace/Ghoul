dd510909662f496a6da4a599fa47ecc7d9107df9
Alexander Bock <alexander.bock@liu.se>
2015-12-07 19:52:04 -0500

    Change API of interpolators to use single functions instead of state-less classes

--- include/ghoul/misc/interpolator.h
+++ include/ghoul/misc/interpolator.h
@@ -26,87 +26,37 @@
 #ifndef __INTERPOLATOR_H__
 #define __INTERPOLATOR_H__
 
-#include <array>
-
-namespace ghoul{
-    /**
-     * Enum listing the available template specialization of the Interpolator class.
-     */
-    enum Interpolators {
-        Linear,
-        CatmullRom,
-        Undefined
-    };
-
-    /**
-     * This is a templated class for Interpolation of arbitrary data types.
-     * The particular interpolation method for any given data is defined by
-     * which template specialization is instantiated, for available interpolators
-     * see the <code>Interpolators</code> enum.
-     */
-    template <Interpolators T>
-    class Interpolator{
-    public:
-        
-        /**
-         * Returns an interpolated value based on the factor supplied in <code>fact</code>.
-         * In addition to the interpolation factor <code>fact</code> a minimum of two values
-         * which to interpolate between need to be supplied, <code>p0</code> and <code>p1</code>.
-         * \param fact The interpolation factor to use in interpolation
-         * \param p0 The first control point of the interpolation
-         * \param p1 The second control point of the interpolation
-         * \param arguments An arbitrary number of additional control points to use in interpolation.
-         * \return The interpolated value for the given control points based on
-         * the interpolation factor <code>fact</code>
-         */
-        template<typename U, typename... UArgs>
-        U interpolate(double fact, U p0, U p1, UArgs && ... arguments);
-    };
-
-    /**
-     * This the linear interpolation template specialization of the Interpolator class.
-     */
-    template<>
-    class Interpolator<Interpolators::Linear>{
-    public:
-        /**
-         * Returns an linearly interpolated value between the control points <code>p0</code>
-         * and <code>p1</code> based on the interpolation factor <code>fact</code>.
-         * \param fact The interpolation factor to use in interpolation
-         * \param p0 The first control point of the interpolation
-         * \param p1 The second control point of the interpolation
-         * \param arguments An arbitrary number of additional control points, all of which are
-         * ignored in this interpolation method.
-         * \return The linearly interpolated value for the given control points based on
-         * the interpolation factor <code>fact</code>
-         */
-        template<typename U, typename... UArgs>
-        U interpolate(double fact, U p0, U p1, UArgs && ... arguments);
-    };
+namespace ghoul {
     
-    /**
-     * This the Catmull-Rom interpolation template specialization of the Interpolator class.
-     */
-    template<>
-    class Interpolator<Interpolators::CatmullRom>{
-    public:
-        /**
-         * Returns an interpolated value using the Catmull-Rom spline interpolation scheme.
-         * Four conrol points are used in the interpolation; <code>p0</code>, <code>p1</code>
-         * and two points extracted from <code>arguments</code> and interpolation is calculated
-         * based on the interpolation factor <code>fact</code>.
-         * \param fact The interpolation factor to use in interpolation
-         * \param p0 The first control point of the interpolation
-         * \param p1 The second control point of the interpolation
-         * \param arguments Two additional control points used in the interpolation.
-         * \return The interpolated value for the given control points calculated using Catmull-Rom 
-         * spline interpolation based on the interpolation factor <code>fact</code>
-         */
-        template<typename U, typename... UArgs>
-        U interpolate(double fact, U p0, U p1, UArgs && ... arguments);
-    };
-    
-}
+/**
+ * Returns an interpolated value based on the factor supplied in \p t. In addition to the
+ * interpolation factor \p t two values which to interpolate between must be supplied.
+ * For <code>t = 0.0</code>, \p p0 is returned, for <code>t = 1.0</code>, \p p1 is
+ * returned.
+ * \param t The interpolation factor to use in interpolation
+ * \param p0 The first control point of the interpolation
+ * \param p1 The second control point of the interpolation
+ * \return The interpolated value for the given control points based on \t
+ */
+template <typename T>
+T interpolateLinear(double t, const T& p0, const T& p1);
+
+/**
+ * Returns an interpolated value using the Catmull-Rom spline interpolation scheme. Four
+ * control points are used in the interpolation; \p p0, \p p1, \p p2, and \p3. The 
+ * interpolation is calculated based on the interpolation factor \p t.
+ * \param t The interpolation factor to use in interpolation
+ * \param p0 The first control point of the interpolation
+ * \param p1 The second control point of the interpolation
+ * \param p2 The third control point of the interpolation
+ * \param p3 The fourth control point of the interpolation
+ * \return The interpolated value for the given control points calculated using
+ * Catmull-Rom spline interpolation based on the interpolation factor \p t
+ */
+template <typename T>
+T interpolateCatmullRom(double t, const T& p0, const T& p1, const T& p2, const T& p3);
+
+} // namespace ghoul
 
 #include "interpolator.inl"
 
