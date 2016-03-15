93f4d4afacc3f7c335259d962ca2722ed346b448
Alexander Bock <alexander.bock@liu.se>
2016-03-15 23:59:15 +0100

    Removing boost dependency by replacing boost::any file with a native copy

---
+++ include/ghoul/misc/any.h
@@ -0,0 +1,273 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2016                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ *****************************************************************************************
+ * This class is heavily based on the boost::any class written by Kevlin Henny with      *
+ * contibutation from Antony Polukhin, Ed Brey, Mark Rodgers, Peter Dimov, and James     *
+ * Curran.                                                                               *
+ * Copyright Kevlin Henney, 2000, 2001, 2002. All rights reserved.                       *
+ * Original file ddstributed under the Boost Software License, Version 1.0. (See copy at *
+ * http://www.boost.org/LICENSE_1_0.txt)                                                 *
+ ****************************************************************************************/
+
+#ifndef __ANY_H__
+#define __ANY_H__
+
+#include <algorithm>
+#include <type_traits>
+#include <typeinfo>
+
+namespace ghoul {
+   
+/**
+ * An object of type !any is able to represent any object. When accessing the stored value
+ * using !any_cast%, the correct type must be requested or an exception is thrown.
+ */
+class any {
+public:
+    /// Constructor creating an empty any object
+    any() noexcept;
+
+    /**
+     * Constructor creating an !any object from the \p value.
+     * \param value The value that should be stored in the any object
+     */ 
+    template<typename ValueType>
+    any(const ValueType& value);
+
+    /**
+     * Constructor duplicating the contents of another any object. The specific type is
+     * not needed for this function
+     */
+    any(const any& other);
+
+    /**
+     * Move constructor that initializes this !any object's contents with the passed
+     * object, leaving it uninitialized in the process.
+     * \param other The object used to create this !any object
+     */
+    any(any&& other) noexcept;
+
+    /**
+     * If the \p value is of a correct type, we can use perfect forwarding for efficiency
+     * reasons when move-constructing an !any object.
+     * \param value The value from which to create this !any object
+     */
+    template <typename ValueType>
+    any(ValueType&& value,
+        typename std::enable_if_t<!std::is_same<any&, ValueType>::value>* = 0,
+        typename std::enable_if_t<!std::is_const<ValueType>::value>* = 0);
+
+    /// Default destructor 
+    ~any() noexcept;
+
+    /**
+     * Swaps the contents of \p rhs and <code>this</code> object.
+     * \param rhs The !any object which which <code>this</code> will swap its contents
+     */
+    any& swap(any& rhs) noexcept;
+
+    /**
+     * Default assignment operator.
+     * \param rhs The value that should be assigned to <code>this</code>
+     */
+    any& operator=(const any& rhs);
+
+    /**
+     * Standard move operator that leaves <code>this</code> !any object in an emtpy state.
+     * \param rhs The source from which to move-assign <code>this</code> !any object
+     */
+    any& operator=(any&& rhs) noexcept;
+
+    /**
+     * Assigns the values of \p rhs to <code>this</code> !any object by perfectly
+     * forwarding the contents.
+     * \param rhs The value used to move-assign to <code>this</code> any object
+     */
+    template <typename ValueType>
+    any& operator=(ValueType&& rhs);
+
+    /**
+     * Returns <code>true</code> if this !any object is unassigned, deleting any stored
+     * content in the process.
+     * \return <code>true</code> if this !any object is unassigned, <code>false</code>
+     * otherwise
+     */
+    bool empty() const noexcept;
+
+    /**
+     * Clears this !any object and makes leaves it empty.
+     */
+    void clear() noexcept;
+
+    /**
+     * Returns the std::type_info of the stored value.
+     * \return The std::type_info of the stored value
+     */
+    const std::type_info& type() const noexcept;
+
+    /**
+     * This abstract internal class is a baseclass to !holder, which stores the contents
+     * for an !any object.
+    */
+    class placeholder {
+    public:
+        /// Default virtual destructor
+        virtual ~placeholder();
+
+        /**
+         * This method returns the std::type_info of the stored value
+         * \returns The std::type_info of the stored value
+         */
+        virtual const std::type_info& type() const noexcept = 0;
+
+        /**
+         * Creates a copy of the stored value by calling its copy constructor
+         * \return A copy of the stored value by calling its copy constructor
+         */
+        virtual placeholder* clone() const = 0;
+    };
+
+    /**
+     * The concrete implementation of a placeholder using a specific type of value held.
+     * The <code>ValueType</code> must be copy constructable and move constructable.
+     */
+    template <typename ValueType>
+    class holder : public placeholder {
+    public:
+        /**
+         * Constructor that will take a copy of the \p value and stores it internally.
+         * \param value The value to be stored in the holder
+         */
+        holder(const ValueType& value);
+
+        /**
+         * Move constructor that will store the contents of \p value inside and leave the
+         * \p value in an undefined state.
+         * \param value The value that is used to initialize this holder
+         */
+        holder(ValueType&& value);
+
+        /**
+         * Returns the std::type_info struct that contains the type information for the
+         * stored value type. This call is equivalent to calling
+         * <code>typeid(ValueType)</code>.
+         * \return The std::type_info structure describing the stored value's type
+         */
+        const std::type_info& type() const noexcept override;
+
+        /**
+         * Clones the stored value by invoking the copy constructor of
+         * <code>ValueType</code>.
+         * \return A copy of the stored value
+         */
+        placeholder* clone() const override;
+
+        /// The object that is contained in this !holder
+        ValueType held;
+
+    private:
+        holder& operator=(const holder&) = delete;
+
+    };
+
+    placeholder* content;
+};
+
+/**
+ * And exception of this class is thrown if an any_cast fails due to mismatching types.
+ */
+class bad_any_cast : public std::bad_cast {
+public:
+    const char* what() const noexcept override; 
+};
+
+/**
+ * This method casts an !any object \p operand into a type <code>ValueType</code> and
+ * returns it.
+ * \param operand The !any object that is to be converted into the type
+ * <code>ValueType</code>
+ * \return The converted \p operand
+ * \throw bad_any_cast If the \p operand does not contain a value of type
+ * <code>ValueType</code>
+ * \pre \p operand must not be <code>nullptr</code>
+ * \post The return value is not <code>nullptr</code>
+ */
+template <typename ValueType>
+ValueType* any_cast(any* operand) noexcept;
+
+/**
+* This method casts an !any object \p operand into a type <code>ValueType</code> and
+* returns it.
+* \param operand The !any object that is to be converted into the type
+* <code>ValueType</code>
+* \return The converted \p operand
+* \throw bad_any_cast If the \p operand does not contain a value of type
+* <code>ValueType</code>
+* \pre \p operand must not be <code>nullptr</code>
+* \post The return value is not <code>nullptr</code>
+*/
+template<typename ValueType>
+inline const ValueType* any_cast(const any* operand) noexcept;
+
+/**
+* This method casts an !any object \p operand into a type <code>ValueType</code> and
+* returns it.
+* \param operand The !any object that is to be converted into the type
+* <code>ValueType</code>
+* \return The converted \p operand
+* \throw bad_any_cast If the \p operand does not contain a value of type
+* <code>ValueType</code>
+*/
+template<typename ValueType>
+ValueType any_cast(any& operand);
+
+/**
+* This method casts an !any object \p operand into a type <code>ValueType</code> and
+* returns it.
+* \param operand The !any object that is to be converted into the type
+* <code>ValueType</code>
+* \return The converted \p operand
+* \throw bad_any_cast If the \p operand does not contain a value of type
+* <code>ValueType</code>
+*/
+template<typename ValueType>
+inline ValueType any_cast(const any& operand);
+
+/**
+* This method casts an !any object \p operand into a type <code>ValueType</code> and
+* returns it.
+* \param operand The !any object that is to be converted into the type
+* <code>ValueType</code>
+* \return The converted \p operand
+* \throw bad_any_cast If the \p operand does not contain a value of type
+* <code>ValueType</code>
+*/
+template<typename ValueType>
+inline ValueType any_cast(any&& operand);
+
+} // namespace
+
+#include "any.inl"
+
+#endif // __ANY_H__
---
+++ include/ghoul/misc/any.inl
@@ -0,0 +1,148 @@
+/*****************************************************************************************
+*                                                                                       *
+* GHOUL                                                                                 *
+* General Helpful Open Utility Library                                                  *
+*                                                                                       *
+* Copyright (c) 2012-2016                                                               *
+*                                                                                       *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+* software and associated documentation files (the "Software"), to deal in the Software *
+* without restriction, including without limitation the rights to use, copy, modify,    *
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+* permit persons to whom the Software is furnished to do so, subject to the following   *
+* conditions:                                                                           *
+*                                                                                       *
+* The above copyright notice and this permission notice shall be included in all copies *
+* or substantial portions of the Software.                                              *
+*                                                                                       *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+****************************************************************************************/
+
+#include <ghoul/misc/assert.h>
+
+namespace ghoul {
+
+template<typename ValueType>
+any::any(const ValueType& value)
+    : content(new holder<typename std::remove_cv_t<typename std::decay_t<const ValueType>>
+        >(value))
+{}
+
+template <typename ValueType>
+any::any(ValueType&& value,
+    // disable if value has type `any&`
+    typename std::enable_if_t<!std::is_same<any&, ValueType>::value>*, 
+    // disable if value has type `const ValueType&&`
+    typename std::enable_if_t<!std::is_const<ValueType>::value>*)  
+    : content(new holder<
+                typename std::decay_t<ValueType>>(static_cast<ValueType&&>(value)))
+{}
+
+template <typename ValueType>
+any& any::operator=(ValueType&& rhs) {
+    any(static_cast<ValueType&&>(rhs)).swap(*this);
+    return *this;
+}
+
+template <typename ValueType>
+any::holder<ValueType>::holder(const ValueType & value)
+    : held(value)
+{}
+
+template <typename ValueType>
+any::holder<ValueType>::holder(ValueType&& value)
+    : held(static_cast<ValueType&&>(value))
+{}
+
+template <typename ValueType>
+const std::type_info& any::holder<ValueType>::type() const noexcept {
+    return typeid(ValueType);
+}
+
+template<typename ValueType>
+any::placeholder* any::holder<ValueType>::clone() const {
+    return new holder(held);
+}
+
+template <typename ValueType>
+ValueType* any_cast(any* operand) noexcept {
+    ghoul_assert(operand, "Operand for any_cast must not be nullptr");
+    if (operand->type() == typeid(ValueType)) {
+        return &static_cast<
+            any::holder<typename std::remove_cv_t<ValueType>>*
+        >(operand->content)->held;
+    }
+    else
+        throw bad_any_cast();
+}
+
+template <typename ValueType>
+inline const ValueType* any_cast(const any* operand) noexcept {
+    ghoul_assert(operand, "Operand for any_cast must not be nullptr");
+    if (operand->type() == typeid(ValueType)) {
+        return &static_cast<
+            any::holder<typename std::remove_cv_t<ValueType>>*
+        >(operand->content)->held;
+    }
+    else
+        throw bad_any_cast();
+}
+
+template<typename ValueType>
+ValueType any_cast(any& operand) {
+    typedef typename std::remove_reference<ValueType>::type nonref;
+
+    nonref* result = any_cast<nonref>(&operand);
+    if (!result)
+        throw bad_any_cast();
+
+    // Attempt to avoid construction of a temporary object in cases when 
+    // `ValueType` is not a reference. Example:
+    // `static_cast<std::string>(*result);` 
+    // which is equal to `std::string(*result);`
+    //typedef typename boost::mpl::if_<
+    //    boost::is_reference<ValueType>,
+    //    ValueType,
+    //    BOOST_DEDUCED_TYPENAME boost::add_reference<ValueType>::type
+    //>::type ref_type;
+
+    return static_cast<ValueType>(*result);
+}
+
+template<typename ValueType>
+inline ValueType any_cast(const any& operand) {
+    typedef typename std::remove_reference<ValueType>::type nonref;
+
+    nonref* result = any_cast<nonref>(&operand);
+    if (!result)
+        throw bad_any_cast();
+
+    // Attempt to avoid construction of a temporary object in cases when 
+    // `ValueType` is not a reference. Example:
+    // `static_cast<std::string>(*result);` 
+    // which is equal to `std::string(*result);`
+    //typedef typename boost::mpl::if_<
+    //    boost::is_reference<ValueType>,
+    //    ValueType,
+    //    BOOST_DEDUCED_TYPENAME boost::add_reference<ValueType>::type
+    //>::type ref_type;
+
+    return static_cast<ValueType>(*result);
+}
+
+template<typename ValueType>
+inline ValueType any_cast(any&& operand) {
+    static_assert(
+        std::is_rvalue_reference<ValueType&&>::value /*true if ValueType is rvalue or just a value*/
+        || std::is_const< typename std::remove_reference<ValueType>::type >::value,
+        "ghoul::any_cast shall not be used for getting nonconst references to temporary objects"
+        );
+    return any_cast<ValueType>(operand);
+}
+
+} // namespace
