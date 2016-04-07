cf83268bdde734c97eb22860959b226c02ea7b82
Alexander Bock <alexander.bock@liu.se>
2016-04-07 19:05:38 +0200

    Remove automatic conversion of luminance images into RGB images from TextureReaderDevil Provide function to convert Texture objects between different formats

--- src/io/texture/texturereaderdevil.cpp
+++ src/io/texture/texturereaderdevil.cpp
@@ -36,29 +36,22 @@
 #include <IL/il.h>
 #include <IL/ilu.h>
 
-namespace {
-	const std::string _loggerCat = "TextureReaderDevIL";
-}
-
 namespace ghoul {
 namespace io {
 
 std::unique_ptr<opengl::Texture> TextureReaderDevIL::loadTexture(std::string filename) const {
-	using opengl::Texture;
+    using opengl::Texture;
     ilInit();
     iluInit();
-    
-    //ilOriginFunc(IL_ORIGIN_LOWER_LEFT);
-    //ilEnable(IL_ORIGIN_SET);
 
     ILboolean loadSuccess = ilLoadImage(filename.c_str());
     if (!loadSuccess) {
         ILenum error = ilGetError();
-		throw TextureLoadException(
-			std::move(filename),
-			fmt::format("Error loading image: {}", iluErrorString(error)),
-			this
-		);
+        throw TextureLoadException(
+            std::move(filename),
+            fmt::format("Error loading image: {}", iluErrorString(error)),
+            this
+        );
     }
     ILint imageFormat = ilGetInteger(IL_IMAGE_FORMAT);
 
@@ -68,10 +61,10 @@ std::unique_ptr<opengl::Texture> TextureReaderDevIL::loadTexture(std::string fil
     ILint height = ilGetInteger(IL_IMAGE_HEIGHT);
     ILint depth = ilGetInteger(IL_IMAGE_DEPTH);
 
-    if (imageFormat == IL_LUMINANCE || imageFormat == IL_COLOUR_INDEX) {
-        imageFormat = IL_RGB;
-        imageByte *= 3;
-    }
+    //if (imageFormat == IL_LUMINANCE || imageFormat == IL_COLOUR_INDEX) {
+    //    imageFormat = IL_RGB;
+    //    imageByte *= 3;
+    //}
 
     // Copy data from common data store to own address space
     ILubyte* data = new ILubyte[width * height * imageByte];
@@ -87,21 +80,20 @@ std::unique_ptr<opengl::Texture> TextureReaderDevIL::loadTexture(std::string fil
         case IL_RGBA:
             format = Texture::Format::RGBA;
             break;
-        case IL_BGR:
-            format = Texture::Format::BGR;
+        case IL_LUMINANCE:
+            format = Texture::Format::Red;
             break;
-        case IL_BGRA:
-            format = Texture::Format::BGRA;
+        case IL_LUMINANCE_ALPHA:
+            format = Texture::Format::RG;
             break;
         default:
-			throw TextureLoadException(
-				std::move(filename),
-				fmt::format("Error reading format: {}", imageFormat),
-				this
-			);
+            throw TextureLoadException(
+                std::move(filename),
+                fmt::format("Error reading format: {}", imageFormat),
+                this
+            );
     }
 
-    
     GLenum type;
     switch (imageType) {
         case IL_UNSIGNED_BYTE:
@@ -126,155 +118,149 @@ std::unique_ptr<opengl::Texture> TextureReaderDevIL::loadTexture(std::string fil
             type = GL_FLOAT;
             break;
         default:
-			throw TextureLoadException(
-				std::move(filename),
-				fmt::format("Error reading data type: {}", imageType),
-				this
-			);
+            throw TextureLoadException(
+                std::move(filename),
+                fmt::format("Error reading data type: {}", imageType),
+                this
+            );
     }
 
-	return std::make_unique<Texture>(data, size, format, static_cast<int>(format), type);
+    return std::make_unique<Texture>(data, size, format, static_cast<int>(format), type);
 }
 
 std::unique_ptr<opengl::Texture> TextureReaderDevIL::loadTextureFromMemory(const std::string& buffer) const {
-	using opengl::Texture;
-	ilInit();
-	iluInit();
+    using opengl::Texture;
+    ilInit();
+    iluInit();
 
-	//ilOriginFunc(IL_ORIGIN_LOWER_LEFT);
-	//ilEnable(IL_ORIGIN_SET);
+    //ilOriginFunc(IL_ORIGIN_LOWER_LEFT);
+    //ilEnable(IL_ORIGIN_SET);
 
-	ILboolean loadSuccess = ilLoadL(IL_TYPE_UNKNOWN, (ILubyte*)buffer.c_str(), buffer.size());
-	if (!loadSuccess) {
-		ILenum error = ilGetError();
-		throw TextureLoadException(
-			"Memory",
-			fmt::format("Error loading image: {}", iluErrorString(error)),
-			this
-			);
-	}
-	ILint imageFormat = ilGetInteger(IL_IMAGE_FORMAT);
+    ILboolean loadSuccess = ilLoadL(IL_TYPE_UNKNOWN, (ILubyte*)buffer.c_str(), buffer.size());
+    if (!loadSuccess) {
+        ILenum error = ilGetError();
+        throw TextureLoadException(
+            "Memory",
+            fmt::format("Error loading image: {}", iluErrorString(error)),
+            this
+            );
+    }
+    ILint imageFormat = ilGetInteger(IL_IMAGE_FORMAT);
 
-	ILint imageType = ilGetInteger(IL_IMAGE_TYPE);
-	ILint imageByte = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
-	ILint width = ilGetInteger(IL_IMAGE_WIDTH);
-	ILint height = ilGetInteger(IL_IMAGE_HEIGHT);
-	ILint depth = ilGetInteger(IL_IMAGE_DEPTH);
+    ILint imageType = ilGetInteger(IL_IMAGE_TYPE);
+    ILint imageByte = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
+    ILint width = ilGetInteger(IL_IMAGE_WIDTH);
+    ILint height = ilGetInteger(IL_IMAGE_HEIGHT);
+    ILint depth = ilGetInteger(IL_IMAGE_DEPTH);
 
-	if (imageFormat == IL_LUMINANCE || imageFormat == IL_COLOUR_INDEX) {
-		imageFormat = IL_RGB;
-		imageByte *= 3;
-	}
+    if (imageFormat == IL_LUMINANCE || imageFormat == IL_COLOUR_INDEX) {
+        imageFormat = IL_RGB;
+        imageByte *= 3;
+    }
 
-	// Copy data from common data store to own address space
-	ILubyte* data = new ILubyte[width * height * imageByte];
-	ilCopyPixels(0, 0, 0, width, height, 1, imageFormat, IL_UNSIGNED_BYTE, data);
+    // Copy data from common data store to own address space
+    ILubyte* data = new ILubyte[width * height * imageByte];
+    ilCopyPixels(0, 0, 0, width, height, 1, imageFormat, IL_UNSIGNED_BYTE, data);
 
-	glm::size3_t size(width, height, depth);
+    glm::size3_t size(width, height, depth);
 
-	Texture::Format format;
-	switch (imageFormat) {
-	case IL_RGB:
-		format = Texture::Format::RGB;
-		break;
-	case IL_RGBA:
-		format = Texture::Format::RGBA;
-		break;
-	case IL_BGR:
-		format = Texture::Format::BGR;
-		break;
-	case IL_BGRA:
-		format = Texture::Format::BGRA;
-		break;
-	default:
-		throw TextureLoadException(
-			"Memory",
-			fmt::format("Error reading format: {}", imageFormat),
-			this
-			);
-	}
+    Texture::Format format;
+    switch (imageFormat) {
+    case IL_RGB:
+        format = Texture::Format::RGB;
+        break;
+    case IL_RGBA:
+        format = Texture::Format::RGBA;
+        break;
+    default:
+        throw TextureLoadException(
+            "Memory",
+            fmt::format("Error reading format: {}", imageFormat),
+            this
+            );
+    }
 
 
-	GLenum type;
-	switch (imageType) {
-	case IL_UNSIGNED_BYTE:
-		type = GL_UNSIGNED_BYTE;
-		break;
-	case IL_BYTE:
-		type = GL_BYTE;
-		break;
-	case IL_UNSIGNED_SHORT:
-		type = GL_UNSIGNED_SHORT;
-		break;
-	case IL_SHORT:
-		type = GL_SHORT;
-		break;
-	case IL_UNSIGNED_INT:
-		type = GL_UNSIGNED_INT;
-		break;
-	case IL_INT:
-		type = GL_INT;
-		break;
-	case IL_FLOAT:
-		type = GL_FLOAT;
-		break;
-		default:
-		throw TextureLoadException(
-			"Memory",
-			fmt::format("Error reading data type: {}", imageType),
-			this
-			);
-	}
+    GLenum type;
+    switch (imageType) {
+    case IL_UNSIGNED_BYTE:
+        type = GL_UNSIGNED_BYTE;
+        break;
+    case IL_BYTE:
+        type = GL_BYTE;
+        break;
+    case IL_UNSIGNED_SHORT:
+        type = GL_UNSIGNED_SHORT;
+        break;
+    case IL_SHORT:
+        type = GL_SHORT;
+        break;
+    case IL_UNSIGNED_INT:
+        type = GL_UNSIGNED_INT;
+        break;
+    case IL_INT:
+        type = GL_INT;
+        break;
+    case IL_FLOAT:
+        type = GL_FLOAT;
+        break;
+        default:
+        throw TextureLoadException(
+            "Memory",
+            fmt::format("Error reading data type: {}", imageType),
+            this
+            );
+    }
 
-	return std::make_unique<Texture>(data, size, format, static_cast<int>(format), type);
+    return std::make_unique<Texture>(data, size, format, static_cast<int>(format), type);
 }
 
 std::vector<std::string> TextureReaderDevIL::supportedExtensions() const {
-	// Taken from http://openil.sourceforge.net/features.php
-	return {
-		"bmp",						// Windows Bitmap
-		"cut",						// Dr. Halo
-		"dcx",						// Multi-PCX
-		"dicom", "dcm",				// Dicom
-		"dds",						// DirectDraw Surface
-		"exr",						// OpenEXR
-		"fits", "fit",				// Flexible Image Transport System
-		"ftx",						// Heavy Metal: FAKK 2
-		"hdr",						// Radiance High Dynamic
-		"icns",						// Macintosh icon
-		"ico", "cur",				// Windows icon/cursor
-		"iff",						// Interchange File Format
-		"iwi",						// Infinity Ward Image
-		"gif",						// Graphics Interchange Format
-		"jpg", "jpe", "jpeg",		// Jpeg
-		"jp2",						// Jpeg 2000
-		"lbm",						// Interlaced Bitmap
-		"lif",						// Homeworld texture
-		"mdl",						// Half-Life Model
-		"mp3",						// MPEG-1 Audio Layer 3
-		"pal",						// Palette
-		"pcd",						// Kodak PhotoCD
-		"pcx",						// ZSoft PCX
-		"pic",						// Softimage PIC
-		"png",						// Portable Network Graphics
-		"pbm", "pgm", "pnm",		// Portable Anymap
-		"pix",						// Alias | Wavefront
-		"psd",						// Adobe PhotoShop
-		"psp",						// PaintShop Pro
-		"pxr",						// Pixar
-		"raw",						// Raw data
-		"rot",						// Homeworld 2 Texture
-		"sgi", "bw", "rgb", "rgba"	// Silicon Graphics
-		"texture",					// Creative Assembly Texture
-		"tga",						// Truevision Targa
-		"tif", "tiff",				// Tagged Image File Format
-		"tpl",						// Gamecube Texture
-		"utx",						// Unreal Texture
-		"wal",						// Quake 2 Texture
-		"vtf",						// Valve Texture Format
-		"wdp", "hdp",				// HD Photo
-		"xpm"						// X Pixel Map
-	};
+    // Taken from http://openil.sourceforge.net/features.php
+    return{
+        "bmp",						// Windows Bitmap
+        "cut",						// Dr. Halo
+        "dcx",						// Multi-PCX
+        "dicom", "dcm",				// Dicom
+        "dds",						// DirectDraw Surface
+        "exr",						// OpenEXR
+        "fits", "fit",				// Flexible Image Transport System
+        "ftx",						// Heavy Metal: FAKK 2
+        "hdr",						// Radiance High Dynamic
+        "icns",						// Macintosh icon
+        "ico", "cur",				// Windows icon/cursor
+        "iff",						// Interchange File Format
+        "iwi",						// Infinity Ward Image
+        "gif",						// Graphics Interchange Format
+        "jpg", "jpe", "jpeg",		// Jpeg
+        "jp2",						// Jpeg 2000
+        "lbm",						// Interlaced Bitmap
+        "lif",						// Homeworld texture
+        "mdl",						// Half-Life Model
+        "mp3",						// MPEG-1 Audio Layer 3
+        "pal",						// Palette
+        "pcd",						// Kodak PhotoCD
+        "pcx",						// ZSoft PCX
+        "pic",						// Softimage PIC
+        "png",						// Portable Network Graphics
+        "pbm", "pgm", "pnm",		// Portable Anymap
+        "pix",						// Alias | Wavefront
+        "psd",						// Adobe PhotoShop
+        "psp",						// PaintShop Pro
+        "pxr",						// Pixar
+        "raw",						// Raw data
+        "rot",						// Homeworld 2 Texture
+        "sgi", "bw", "rgb", "rgba"	// Silicon Graphics
+        "texture",					// Creative Assembly Texture
+        "tga",						// Truevision Targa
+        "tif", "tiff",				// Tagged Image File Format
+        "tpl",						// Gamecube Texture
+        "utx",						// Unreal Texture
+        "wal",						// Quake 2 Texture
+        "vtf",						// Valve Texture Format
+        "wdp", "hdp",				// HD Photo
+        "xpm"						// X Pixel Map
+    };
 }
 
 } // namespace opengl
