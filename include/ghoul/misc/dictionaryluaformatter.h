b9c7f62adb0feb74256601ab3c5d2fbc667c52e7
Emil Axelsson <emil.axelsson@liu.se>
2016-12-27 10:42:00 +0100

    Add DictionaryLuaFormatter

---
+++ include/ghoul/misc/dictionaryluaformatter.h
@@ -0,0 +1,77 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2016                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#ifndef __GHOUL___DICTIONARYLUAFORMATTER___H__
+#define __GHOUL___DICTIONARYLUAFORMATTER___H__
+
+#include <ghoul/misc/dictionaryformatter.h>
+
+#include <ghoul/misc/exception.h>
+
+namespace ghoul {
+
+class Dictionary;
+
+/**
+ * This concrete implementation of a DictionaryFormatter converts the passed Dictionary
+ * into a Lua format.
+ * 
+ * At the moment, the only types that are supported in this conversion are:
+ * <code>glm::vec2</code>, <code>glm::vec3</code>, <code>glm::vec4</code>,
+ * <code>float</code>, <code>int</code>, and <code>std::string</code>.
+ */
+class DictionaryLuaFormatter : public DictionaryFormatter {
+public:
+    /**
+     * This exception is thrown if an unknown type is being converted.
+     */
+    struct LuaFormattingError : public RuntimeError {
+        explicit LuaFormattingError(const std::string& message);
+    };
+
+    /**
+     * Converts the passed \p dictionary into a JSON string representation.
+     * \param dictionary The Dictionary that should be converted
+     * \return A JSON string representing the Dictionary
+     * \throw JsonFormattingError If the \p key points to a type that cannot be converted
+     */
+    std::string format(const Dictionary& dictionary) const override;
+
+private:
+    /**
+     * Converts a single value \p key out of the \p dictionary by manually iterating all
+     * the types and trying to access them.
+     * \param dictionary The Dictionary from which the \p key should be extracted and
+     * converted
+     * \param key The key in the Dictionary that should be converted
+     * \return A JSON representation of the \p key's value
+     * \throw JsonFormattingError If the \p key points to a type that cannot be converted
+     */
+    std::string formatValue(const Dictionary& dictionary, const std::string& key) const;
+};
+
+}  // namespace ghoul
+
+#endif // __GHOUL___DICTIONARYLUAFORMATTER___H__
---
+++ src/misc/dictionaryluaformatter.cpp
@@ -0,0 +1,143 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2016                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#include <ghoul/misc/dictionaryluaformatter.h>
+#include <ghoul/misc/dictionary.h>
+
+#include <glm/glm.hpp>
+
+#include <numeric>
+#include <string>
+
+namespace ghoul {
+
+std::string DictionaryLuaFormatter::format(const Dictionary& dictionary) const {
+    if (dictionary.empty()) {
+        return "{}";
+    }
+
+    auto convert = [this, dictionary](const std::string& key) -> std::string {
+        return key + " = " + formatValue(dictionary, key);
+    };
+
+    std::vector<std::string> keys = dictionary.keys();
+    
+    std::string lua = std::accumulate(
+        std::next(keys.begin()),
+        keys.end(),
+        convert(*keys.begin()),
+        [convert](std::string a, std::string key) {
+            return a + "," + convert(key);
+        }
+    );
+
+    return "{" + lua + "}";
+}
+
+std::string DictionaryLuaFormatter::formatValue(const Dictionary& dictionary,
+                                                 const std::string& key) const
+{
+    if (dictionary.hasValue<Dictionary>(key)) {
+        Dictionary subDictionary = dictionary.value<Dictionary>(key);
+        return format(subDictionary);
+    }
+
+    if (dictionary.hasValue<glm::vec4>(key)) {
+        glm::vec4 vec = dictionary.value<glm::vec4>(key);
+        return "{" + std::to_string(vec.x) + "," +
+            std::to_string(vec.y) + "," +
+            std::to_string(vec.z) + "," +
+            std::to_string(vec.w) + "}";
+    }
+
+    if (dictionary.hasValue<glm::vec3>(key)) {
+        glm::vec3 vec = dictionary.value<glm::vec3>(key);
+        return "{" + std::to_string(vec.x) + "," +
+            std::to_string(vec.y) + "," +
+            std::to_string(vec.z) + "}";
+    }
+
+    if (dictionary.hasValue<glm::vec2>(key)) {
+        glm::vec2 vec = dictionary.value<glm::vec2>(key);
+        return "{" + std::to_string(vec.x) + "," +
+            std::to_string(vec.y) + "}";
+    }
+
+    if (dictionary.hasValue<float>(key)) {
+        float value = dictionary.value<float>(key);
+        return std::to_string(value);
+    }
+
+    if (dictionary.hasValue<int>(key)) {
+        int value = dictionary.value<int>(key);
+        return std::to_string(value);
+    }
+
+    if (dictionary.hasValue<std::string>(key)) {
+        std::string value = dictionary.value<std::string>(key);
+
+        std::string luaString = "";
+        for (const char& c : value) {
+            switch (c) {
+            case '"':
+                luaString += "\\\"";
+                break;
+            case '\\':
+                luaString += "\\\\";
+                break;
+            case '\b':
+                luaString += "\\b";
+                break;
+            case '\f':
+                luaString += "\\f";
+                break;
+            case '\n':
+                luaString += "\\n";
+                break;
+            case '\r':
+                luaString += "\\r";
+                break;
+            case '\t':
+                luaString += "\\t";
+                break;
+            default:
+                luaString += c;
+            }
+        }
+
+        return "\"" + luaString + "\"";
+    }
+
+    throw LuaFormattingError(
+        "Key '" + key + "' has invalid type for formatting dictionary as json"
+    );
+}
+
+DictionaryLuaFormatter::LuaFormattingError::LuaFormattingError(
+    const std::string& message)
+    : RuntimeError(message, "Dictionary")
+{}
+
+}  // namespace ghoul
