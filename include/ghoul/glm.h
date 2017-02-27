302fca93671f67464393f9b2059717e00a516002
Alexander Bock <alexander.bock@me.com>
2017-02-27 11:10:24 -0500

    Update Ghoul repository

--- include/ghoul/glm.h
+++ include/ghoul/glm.h
@@ -48,10 +48,6 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #endif // GLM_ENABLE_EXPERIMENTAL
 
-#ifndef GLM_FORCE_CXX14
-#define GLM_FORCE_CXX14
-#endif // GLM_FORCE_CXX14
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
