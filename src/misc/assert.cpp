d490fbaf595954515e7fa3c2ba7ed91e6e20cc02
Alexander Bock <alexander.bock@me.com>
2017-03-11 11:26:12 -0500

    Fix spelling error in Assertion

--- src/misc/assert.cpp
+++ src/misc/assert.cpp
@@ -74,7 +74,7 @@ void internal_assert(std::string expression, std::string message, std::string fi
             << padding << message << std::endl;
 
         while (true) {
-            std::cerr << "(I)gnore / Ignore (P)ermanently / (A)ssert/ (E)xit: ";
+            std::cerr << "(I)gnore / Ignore (P)ermanently / (A)ssertion / (E)xit: ";
             std::string inputLine;
             std::getline(std::cin, inputLine);
 
