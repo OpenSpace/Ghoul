598d54bc70f8248fa88eec5793c03be1bf15fdd0
Alexander Bock <alexander.bock@me.com>
2017-04-18 13:57:35 -0400

    Feature/opengldebug (#11)
    * - Add functions to manipulate the OpenGL debug context state
    - Add templated ghoul::from_string equivalent to std::to_string
    
    * Add documentation
    
    * Fix compile fix in GCC and Clang
    
    * Fix for GCC and Clang
    
    * Add comment for platform-dependent code
    Fix switch indentations

---
+++ include/ghoul/opengl/debugcontext.h
@@ -0,0 +1,238 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2017                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#ifndef __GHOUL___DEBUGCONTEXT___H__
+#define __GHOUL___DEBUGCONTEXT___H__
+
+#include <ghoul/misc/boolean.h>
+#include <ghoul/misc/fromstring.h>
+#include <ghoul/opengl/ghoul_gl.h>
+
+#include <string>
+#include <vector>
+
+namespace ghoul {
+namespace opengl {
+namespace debug {
+
+/// A boolean value determining whether we want to enable or disable the debug output
+using DebugOutput = ghoul::Boolean;
+/// A boolean value determining whether we want to force synchronous output 
+using SynchronousOutput = ghoul::Boolean;
+/// A boolean value determining whether to enable individual or groups of messages
+using Enabled = ghoul::Boolean;
+
+/**
+ * The different sources from which a debug message in OpenGL can originate.
+ */
+enum class Source : GLenum {
+    /// Originating from the OpenGL API itself
+    API = GL_DEBUG_SOURCE_API,
+    /// Originating from the windowing system
+    WindowSystem = GL_DEBUG_SOURCE_WINDOW_SYSTEM,
+    /// Originating from the shader compiler
+    ShaderCompiler = GL_DEBUG_SOURCE_SHADER_COMPILER,
+    /// Originating from a third party library that is not the application itself
+    ThirdParty = GL_DEBUG_SOURCE_THIRD_PARTY,
+    /// Originating from the application managing the OpenGL context
+    Application = GL_DEBUG_SOURCE_APPLICATION,
+    /// Originating from other sources
+    Other = GL_DEBUG_SOURCE_OTHER,
+    /// Used in the setDebugMessageControl functions to refer to any source
+    DontCare = GL_DONT_CARE
+};
+
+/**
+ * The different types of messages that can be raised by the OpenGL driver. Descriptions
+ * for individual values are taken from:
+ * https://www.khronos.org/registry/OpenGL/extensions/KHR/KHR_debug.txt
+ */
+enum class Type : GLenum {
+    /// Events that generated an error
+    Error = GL_DEBUG_TYPE_ERROR,
+    /// Behavior that has been marked for deprecation
+    Deprecated = GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR,
+    /// Behavior that is undefined according to the specification
+    Undefined = GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR,
+    /// Use of extensions or shaders in a way that is highly vendor-specific
+    Portability = GL_DEBUG_TYPE_PORTABILITY,
+    /// Implementation-dependent performance warnings
+    Performance = GL_DEBUG_TYPE_PERFORMANCE,
+#ifdef GL_DEBUG_TYPE_MARKER
+    // These three values are not available on OpenGL prior to 4.3
+    /// Annotation of the command stream
+    Marker = GL_DEBUG_TYPE_MARKER,
+    /// Entering a debug group
+    PushGroup = GL_DEBUG_TYPE_PUSH_GROUP,
+    /// Leaving a debug group
+    PopGroup = GL_DEBUG_TYPE_POP_GROUP,
+#endif // GL_DEBUG_TYPE_MARKER
+    Other = GL_DEBUG_TYPE_OTHER,
+    /// Used in the setDebugMessageControl functions to refer to any type
+    DontCare = GL_DONT_CARE
+};
+
+/**
+ * The severity of the emitted message. Descriptions for individual values are taken from:
+ * https://www.khronos.org/registry/OpenGL/extensions/KHR/KHR_debug.txt
+ */
+enum class Severity : GLenum {
+    /// Any GL error; dangerous undefined behavior; any GLSL or ARB shader compiler and
+    /// linker errors
+    High = GL_DEBUG_SEVERITY_HIGH,
+    /// Severe performance warnings; GLSL or other shader compiler and linker warnings;
+    ///use of currently deprecated behavior
+    Medium = GL_DEBUG_SEVERITY_MEDIUM,
+    /// Performance warnings from redundant state changes; trivial undefined behavior
+    Low = GL_DEBUG_SEVERITY_LOW,
+    /// Any message which is not an error or performance concern
+    Notification = GL_DEBUG_SEVERITY_NOTIFICATION,
+    /// Used in the setDebugMessageControl functions to refer to any severity
+    Dontcare = GL_DONT_CARE
+};
+
+/**
+ * Enables the debug context mode for the currently active OpenGL context. Calling this
+ * function outside of a valid OpenGL context is undefined behavior.
+ * \param debug Determines whether the context's debug mode should be enabled or disabled
+ * \param synchronous Determines whether the debug messages are provided synchronously
+ * (i.e. on the same thread as the OpenGL context and in the same stack frame), or whether
+ * debug callbacks can occur at any time.
+ */
+void setDebugOutput(DebugOutput debug, SynchronousOutput synchronous);
+
+/**
+ * Enables or disables debug messages for the provided \p source, \p type, and 
+ * \p severity. Any of these values can be their respective \c Dontcare values if any
+ * value should be modified.
+ * \param source The Source of debug messages that should be modified
+ * \param type The Type of debug messages that should be modified
+ * \param severity The Severity of debug messages that should be modified
+ * \param enabled Whether messages of the specific \p source, \p type, and \p severity
+ * should be enabled or disabled
+ */
+void setDebugMessageControl(Source source, Type type, Severity severity, Enabled enabled);
+
+/**
+ * Enables or disables a set of debug messages for the provided \p source, \p type 
+ * identified by a set of \p identifiers.
+ * \param source The Source of debug messages that should be modified
+ * \param type The Type of debug messages that should be modified
+ * \param identifiers A list of identifiers that should be modified
+ * \param enabled Whether messages of the specific \p source, \p type, and \p severity
+ * should be enabled or disabled
+ * \pre \p source must not be Source::Dontcare
+ * \pre \p type must not be Type::Dontcare
+  */
+void setDebugMessageControl(Source source, Type type,
+    std::vector<unsigned int> identifiers, Enabled enabled);
+
+/**
+ * The callback function that is passed to the setDebugCallback function and that is
+ * called whenever a valid message is reported by the GL.
+ * \param source The source of the debug message
+ * \param type The type of the debug message
+ * \param severity The severity of the debug message
+ * \param id The id of the message
+ * \param message The actually message that was generated by the GL
+ */
+using CallbackFunction = void (*)(Source source, Type type, Severity severity,
+    unsigned int id, std::string message);
+
+/**
+ * Sets the debug callback that is called whenever the GL reports a new debug message.
+ * Only a C-style function pointer or a state-less lambda can be passed.
+ * \param callback The CallbackFunction that is called
+ */
+void setDebugCallback(CallbackFunction callback);
+
+} // namespace debug
+} // namespace opengl
+
+/**
+ * Converts a string with a \p source into a opengl::debug::Source object.
+ * The valid values are "API", "Window System", "Shader Compiler", "Third Party",
+ * "Application", "Other", and "Don't care".
+ * \param source The string containing the name of a opengl::debug::Source
+ * \throw std::out_of_range if the \p source is not a valid name of an
+ * opengl::debug::Source object
+ */
+template <>
+opengl::debug::Source from_string(const std::string& source);
+
+/**
+ * Converts a string with a \p type into a opengl::debug::Source object.
+ * The valid values are "Error", "Deprecated", "Undefined", "Portability",
+ * "Performance", "Marker", "Push group", "Pop group", "Other", and "Don't care".
+ * \param type The string containing the name of a opengl::debug::Type
+ * \throw std::out_of_range if the \p type is not a valid name of an
+ * opengl::debug::Type object
+ */
+template <>
+opengl::debug::Type from_string(const std::string& type);
+
+/**
+* Converts a string with a \p severity into a opengl::debug::Severity object.
+* The valid values are "High", "Medium", "Low", and "Notification".
+* \param severity The string containing the name of a opengl::debug::Severity
+* \throw std::out_of_range if the \p severity is not a valid name of an
+* opengl::debug::Severity object
+*/
+template <>
+opengl::debug::Severity from_string(const std::string& severity);
+
+} // namespace ghoul
+
+namespace std {
+
+/**
+ * Converts the \p source object into its string representation.
+ * The valid return values are: "API", "Window System", "Shader Compiler", "Third Party",
+ * "Application", "Other", and "Don't care".
+ * \param source The ghoul::opengl::debug::Source that is converted into a string
+ * \return The string representation of the \p source
+ */
+std::string to_string(ghoul::opengl::debug::Source source);
+
+/**
+ * Converts the \p type object into its string representation.
+ * The valid return values are: "Error", "Deprecated", "Undefined", "Portability",
+ * "Performance", "Marker", "Push group", "Pop group", "Other", and "Don't care".
+ * \param type The ghoul::opengl::debug::Type that is converted into a string
+ * \return The string representation of the \p type
+ */
+std::string to_string(ghoul::opengl::debug::Type type);
+
+/**
+ * Converts the \p severity object into its string representation.
+ * The valid return values are: "High", "Medium", "Low", and "Notification".
+ * \param severity The ghoul::opengl::debug::Severity that is converted into a string
+ * \return The string representation of the \p severity
+ */
+std::string to_string(ghoul::opengl::debug::Severity severity);
+
+} // namespace std
+
+#endif // __GHOUL___DEBUGCONTEXT___H__
---
+++ src/opengl/debugcontext.cpp
@@ -0,0 +1,225 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2017                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#include <ghoul/opengl/debugcontext.h>
+
+#include <ghoul/misc/assert.h>
+
+#include <map>
+#include <type_traits>
+
+namespace std {
+std::string to_string(ghoul::opengl::debug::Source source) {
+    switch (source) {
+    case ghoul::opengl::debug::Source::API:             return "API";
+    case ghoul::opengl::debug::Source::WindowSystem:    return "Window System";
+    case ghoul::opengl::debug::Source::ShaderCompiler:  return "Shader Compiler";
+    case ghoul::opengl::debug::Source::ThirdParty:      return "Third Party";
+    case ghoul::opengl::debug::Source::Application:     return "Application";
+    case ghoul::opengl::debug::Source::Other:           return "Other";
+    case ghoul::opengl::debug::Source::DontCare:        return "Don't care";
+    default:                                    ghoul_assert(false, "Missing case label");
+    }
+}
+
+std::string to_string(ghoul::opengl::debug::Type type) {
+    switch (type) {
+    case ghoul::opengl::debug::Type::Error:         return "Error";
+    case ghoul::opengl::debug::Type::Deprecated:    return "Deprecated";
+    case ghoul::opengl::debug::Type::Undefined:     return "Undefined";
+    case ghoul::opengl::debug::Type::Portability:   return "Portability";
+    case ghoul::opengl::debug::Type::Performance:   return "Performance";
+    case ghoul::opengl::debug::Type::Marker:        return "Marker";
+    case ghoul::opengl::debug::Type::PushGroup:     return "Push group";
+    case ghoul::opengl::debug::Type::PopGroup:      return "Pop group";
+    case ghoul::opengl::debug::Type::Other:         return "Other";
+    case ghoul::opengl::debug::Type::DontCare:      return "Don't care";
+    default:                                    ghoul_assert(false, "Missing case label");
+    }
+}
+
+std::string to_string(ghoul::opengl::debug::Severity severity) {
+    switch (severity) {
+    case ghoul::opengl::debug::Severity::High:          return "High";
+    case ghoul::opengl::debug::Severity::Medium:        return "Medium";
+    case ghoul::opengl::debug::Severity::Low:           return "Low";
+    case ghoul::opengl::debug::Severity::Notification:  return "Notification";
+    default:                                    ghoul_assert(false, "Missing case label");
+    }
+}
+
+} // namespace std
+
+namespace ghoul {
+
+template <>
+opengl::debug::Source from_string(const std::string& source) {
+    static const std::map<std::string, opengl::debug::Source> Map = {
+        { "API",             opengl::debug::Source::API },
+        { "Window System",   opengl::debug::Source::WindowSystem },
+        { "Shader Compiler", opengl::debug::Source::ShaderCompiler },
+        { "Third Party",     opengl::debug::Source::ThirdParty },
+        { "Application",     opengl::debug::Source::Application },
+        { "Other",           opengl::debug::Source::Other },
+        { "Don't care",      opengl::debug::Source::DontCare }
+    };
+
+    // Throws a std::out_of_range exception if the source could not be found
+    return Map.at(source);
+}
+
+template <>
+opengl::debug::Type from_string(const std::string& type) {
+    static const std::map<std::string, opengl::debug::Type> Map = {
+        { "Error",          opengl::debug::Type::Error },
+        { "Deprecated",     opengl::debug::Type::Deprecated },
+        { "Undefined",      opengl::debug::Type::Undefined },
+        { "Portability",    opengl::debug::Type::Portability },
+        { "Performance",    opengl::debug::Type::Performance },
+        { "Marker",         opengl::debug::Type::Marker },
+        { "Push group",     opengl::debug::Type::PushGroup },
+        { "Pop group",      opengl::debug::Type::PopGroup },
+        { "Other",          opengl::debug::Type::Other },
+        { "Don't care",     opengl::debug::Type::DontCare }
+    };
+
+    // Throws a std::out_of_range exception if the type could not be found
+    return Map.at(type);
+}
+
+template <>
+opengl::debug::Severity from_string(const std::string& severity) {
+    static const std::map<std::string, opengl::debug::Severity> Map = {
+        { "High",           opengl::debug::Severity::High },
+        { "Medium",         opengl::debug::Severity::Medium },
+        { "Low",            opengl::debug::Severity::Low },
+        { "Notification",   opengl::debug::Severity::Notification }
+    };
+
+    // Throws a std::out_of_range exception if the severity could not be found
+    return Map.at(severity);
+}
+
+
+namespace opengl {
+namespace debug {
+
+void setDebugOutput(DebugOutput debug, SynchronousOutput synchronous) {
+    if (debug) {
+        glEnable(GL_DEBUG_OUTPUT);
+    }
+    else {
+        glDisable(GL_DEBUG_OUTPUT);
+    }
+
+    if (synchronous) {
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    }
+    else {
+        glDisable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    }
+}
+
+void setDebugMessageControl(Source source, Type type, Severity severity, Enabled enabled) {
+    glDebugMessageControl(
+        static_cast<std::underlying_type_t<Source>>(source),
+        static_cast<std::underlying_type_t<Type>>(type),
+        static_cast<std::underlying_type_t<Severity>>(severity),
+        0,
+        nullptr,
+        enabled ? GL_TRUE : GL_FALSE
+    );
+}
+
+void setDebugMessageControl(Source source, Type type,
+                            std::vector<unsigned int> identifiers, Enabled enabled)
+{
+    ghoul_assert(source != Source::DontCare, "source must not be Source::Dontcare");
+    ghoul_assert(type != Type::DontCare, "type must not be Type::Dontcare");
+
+    static_assert(
+        std::is_same<unsigned int, GLuint>::value,
+        "We exploit that 'int' and 'GLsizei' are the same for glDebugMessageControl"
+    );
+
+    glDebugMessageControl(
+        static_cast<std::underlying_type_t<Source>>(source),
+        static_cast<std::underlying_type_t<Type>>(type),
+        GL_DONT_CARE,
+        static_cast<GLsizei>(identifiers.size()),
+        identifiers.data(),
+        enabled ? GL_TRUE : GL_FALSE
+    );
+}
+
+//using Callback = void *(Source source, Type type, Severity severity, unsigned int id
+//  std::string message);
+void setDebugCallback(CallbackFunction callback) {
+    // For some reason, the GLEW version I tested with on Windows does not define the
+    // callback correctly (using GLvoid* instead of const GLvoid*), so we have to make
+    // a conditional compilation on that. When (if) this changes, the same callback can
+    // be used on all platforms
+#ifdef WIN32
+    auto internalCallback = [](GLenum source, GLenum type, GLuint id, GLenum severity,
+        GLsizei, const GLchar* message, GLvoid* userParam) -> void
+    {
+        CallbackFunction& cb = *reinterpret_cast<CallbackFunction*>(userParam);
+        cb(
+            Source(source),
+            Type(type),
+            Severity(severity),
+            static_cast<unsigned int>(id),
+            std::string(message)
+        );
+    };
+#else 
+    auto internalCallback = [](GLenum source, GLenum type, GLuint id, GLenum severity,
+        GLsizei, const GLchar* message, const GLvoid* userParam) -> void
+    {
+        const CallbackFunction& cb = *reinterpret_cast<const CallbackFunction*>(
+            userParam
+        );
+
+        cb(
+            Source(source),
+            Type(type),
+            Severity(severity),
+            static_cast<unsigned int>(id),
+            std::string(message)
+        );
+    };
+#endif
+
+    // This looks dangerous, but the callback that is passed in must persist throughout
+    // the lifetime of the program regardless of how it is created.
+    // If it is a regular C-function, it will not be modified anyway, a state-less
+    // lambda expression is the same
+
+    glDebugMessageCallback(internalCallback, &callback);
+}
+
+} // namespace debug
+} // namespace opengl
+} // namespace ghoul
