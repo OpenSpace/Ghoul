4c75bf08d510ca1c694b32a1bfd68553d80e3db0
Alexander Bock <alexander.bock@me.com>
2017-11-06 15:35:46 -0600

    Remove swizzling again

--- include/ghoul/glm.h
+++ include/ghoul/glm.h
@@ -48,9 +48,10 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #endif // GLM_ENABLE_EXPERIMENTAL
 
-#ifndef GLM_SWIZZLE
-#define GLM_SWIZZLE
-#endif // GLM_SWIZZLE
+// Enabling swizzling breaks binary compatibility between glm::vecX and float* ?
+// #ifndef GLM_SWIZZLE
+// #define GLM_SWIZZLE
+// #endif // GLM_SWIZZLE
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
