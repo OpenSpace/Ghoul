c1ed5d5212ca590fb3a889fb04e769e5ec9ef7c2
Alexander Bock <alexander.bock@me.com>
2017-12-29 20:10:25 +0100

    Update copyright header

--- src/ghoul.cpp
+++ src/ghoul.cpp
@@ -3,7 +3,7 @@
  * GHOUL                                                                                 *
  * General Helpful Open Utility Library                                                  *
  *                                                                                       *
- * Copyright (c) 2012-2017                                                               *
+ * Copyright (c) 2012-2018                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
@@ -44,7 +44,7 @@ std::string licenseText() {
     return "GHOUL\n\
 General Helpful Open Utility Library\n\
 \n\
-Copyright(c) 2012 - 2017\n\
+Copyright(c) 2012 - 2018\n\
 \n\
 Permission is hereby granted, free of charge, to any person obtaining a copy of this\n\
 software and associated documentation files(the \"Software\"), to deal in the Software\n\
