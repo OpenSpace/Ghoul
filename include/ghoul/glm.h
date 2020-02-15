473181b4e1e3479ef67ecc855becb32885d1c0b3
Micah Acinapura <micahnyc@gmail.com>
2020-02-14 23:27:56 -0500

    dont force init on macos

--- include/ghoul/glm.h
+++ include/ghoul/glm.h
@@ -50,9 +50,11 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #endif // GLM_ENABLE_EXPERIMENTAL
 
+#ifndef __APPLE__
 #ifndef GLM_FORCE_CTOR_INIT 
 #define GLM_FORCE_CTOR_INIT 
 #endif // GLM_FORCE_CTOR_INIT 
+#endif
 
 // Enabling swizzling breaks binary compatibility between glm::vecX and float* ?
 // #ifndef GLM_SWIZZLE
