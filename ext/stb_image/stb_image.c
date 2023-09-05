c4119699ae11506d34947c0f9b7d2fd877981c9f
Emma Broman <emma.broman@liu.se>
2023-09-05 12:59:32 +0200

    Add new writer class and stb_image_write header

--- ext/stb_image/stb_image.c
+++ ext/stb_image/stb_image.c
@@ -2,3 +2,6 @@
 #define STBI_FAILURE_USERMSG
 
 #include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
---
+++ include/ghoul/io/texture/texturewriterstb.h
@@ -0,0 +1,46 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * GHOUL                                                                                 *
+ * General Helpful Open Utility Library                                                  *
+ *                                                                                       *
+ * Copyright (c) 2012-2023                                                               *
+ *                                                                                       *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+ * software and associated documentation files (the "Software"), to deal in the Software *
+ * without restriction, including without limitation the rights to use, copy, modify,    *
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to the following   *
+ * conditions:                                                                           *
+ *                                                                                       *
+ * The above copyright notice and this permission notice shall be included in all copies *
+ * or substantial portions of the Software.                                              *
+ *                                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ ****************************************************************************************/
+
+#ifndef __GHOUL___TEXTUREWRITERSTB___H__
+#define __GHOUL___TEXTUREWRITERSTB___H__
+
+#include <ghoul/io/texture/texturewriterbase.h>
+
+namespace ghoul::io {
+
+/// Writes the texture using the Sean Barrett's stb_image library.
+class TextureWriterSTB : public TextureWriterBase {
+
+    /**
+     * Returns the supported file extensions.
+     *
+     * \return The supported file extensions
+     */
+    std::vector<std::string> supportedExtensions() const override;
+};
+
+} // namespace ghoul::io
+
+#endif // __GHOUL___TEXTUREWRITERSTB___H__
