c5287a56a8184898cf04c86df17854491b209600
Alexander Bock <alexander.bock@liu.se>
2024-02-06 15:31:32 +0100

    Update copyright header

--- tests/test_luaconversions.cpp
+++ tests/test_luaconversions.cpp
@@ -2,7 +2,7 @@
  *                                                                                       *
  * OpenSpace                                                                             *
  *                                                                                       *
- * Copyright (c) 2012-2023                                                               *
+ * Copyright (c) 2012-2024                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
